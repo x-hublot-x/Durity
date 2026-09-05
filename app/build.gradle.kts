d720e7fb1aa74a3f00815f9a6ca77d21fd08d9fd
x-hublot-x <236365618+x-hublot-x@users.noreply.github.com>
2026-09-05 19:58:02 +0300

    Configure release signing for APK builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
