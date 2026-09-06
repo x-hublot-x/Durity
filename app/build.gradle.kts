e74f64f812d640f55cdb4ad6b4c98535846c95af
x-hublot-x <236365618+x-hublot-x@users.noreply.github.com>
2026-09-06 19:46:35 +0300

    Bump version to 1.1 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.project1"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
